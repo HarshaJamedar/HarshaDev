3808f5c32d26bd0bf03ff68758f46142b76249f0
HarshaJamedar <harsha_jemedar@csu.fullerton.edu>
2024-10-11 20:43:19 -0600

    Added framer motion

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,63 +5,150 @@ import { FaGithub } from "react-icons/fa6"
 import { RiReactjsLine } from "react-icons/ri"
 import { SiApachecassandra, SiApachehadoop, SiApachekafka, SiKubernetes, SiMongodb, SiPython, SiPytorch, SiSpring, SiTensorflow } from "react-icons/si"
 import { TbBrandNextjs } from "react-icons/tb"
+import { motion } from "framer-motion"
+
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repat: Infinity,
+            repeatType: "reverse",
+        }
+    }
+})
 
 
 const Technologies =  () => {
     return (
         <div className="pb-24">
-            <h2 className="my-20 text-center text-4xl">Technologies</h2>
-            <div className="flex flex-wrap items-center justify-center gap-4">
-                <div className="p-4">
-                    <RiReactjsLine className="text-7xl text-cyan-400"></RiReactjsLine>
-                </div>
-                <div className="p-4">
-                    <TbBrandNextjs className="text-7xl"></TbBrandNextjs>
-                </div>
-                <div className="p-4">
+            <motion.h2 
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.5 }}
+            className="my-20 text-center text-4xl">Technologies</motion.h2>
+
+            <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1.5 }}
+            className="flex flex-wrap items-center justify-center gap-4">
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2.5)} 
+                    className="p-4">
+                    <RiReactjsLine className="text-7xl text-cyan-400"/>
+                </motion.div>
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(4)}
+                    className="p-4">
+                    <TbBrandNextjs className="text-7xl" />
+                </motion.div>
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(6)}
+                    className="p-4">
                     <SiMongodb className="text-7xl text-green-400"></SiMongodb>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={iconVariants(2.5)}
+                    className="p-4">
                     <FaNodeJs className="text-7xl text-green-600"></FaNodeJs>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                 className="p-4">
                     <SiSpring className="text-7xl text-green-400"></SiSpring>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(6)}
+                 className="p-4">
                     <DiMysql className="text-7xl"></DiMysql>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2.5)}
+                 className="p-4">
                     <FaAws className="text-7xl"></FaAws>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                className="p-4">
                     <FaDocker className="text-7xl text-blue-500"></FaDocker>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(6)}
+                 className="p-4">
                     <FaGithub className="text-7xl"></FaGithub>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2.5)}
+                 className="p-4">
                     <SiKubernetes className="text-7xl text-blue-700"></SiKubernetes>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                className="p-4">
                     <SiTensorflow className="text-7xl text-orange-400"></SiTensorflow>
-                </div>
-                <div className="p-4"> 
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(6)}
+                className="p-4"> 
                     <SiPytorch className="text-7xl text-red-500"></SiPytorch>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2.5)}
+                className="p-4">
                     <SiPython className="text-7xl"></SiPython>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                className="p-4">
                     <SiApachehadoop className="text-7xl"></SiApachehadoop>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(6)}
+                 className="p-4">
                     <SiApachecassandra className="text-7xl text-blue-300"></SiApachecassandra>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2.5)}
+                className="p-4">
                     <SiApachekafka className="text-7xl"></SiApachekafka>
-                </div>
+                </motion.div>
                 
-            </div>
+            </motion.div>
 
         </div>
     )
