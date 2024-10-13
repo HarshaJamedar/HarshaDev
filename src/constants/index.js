884d496da30fdd6102159c5ce12d5e34f435b092
HarshaJamedar <harsha_jemedar@csu.fullerton.edu>
2024-10-12 21:48:34 -0600

    Changed experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,33 +9,35 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "AUG 2024 - Present",
+    role: "Software Developer Engineer",
+    company: "Bright Mind Enrichment",
+    description: `Team player in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Typescript", "Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "DEC 2022 - MAY 2024",
+    role: "Research Graduate",
+    company: "California State university Fullerton",
+    description: `Developed an autonomous drone motion planning system integrating LiDAR SLAM and YOLO-based computer vision for enhanced navigation and obstacle detection as part of my research project`,
+    technologies: ["ROS", "C++", "Python", "CUDA", "Shell", "ComputerVision", "YOLOV8", "LiDAR", "VSLAM"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    year: "JAN 2020 - MAR 2022",
+    role: "Sr Systems Engineer ",
+    company: "Infosys",
+    description: `Worked as a Systems Engineer, utilizing AWS, Spring Boot, Kafka, Docker and Kubernetes to containerize applications, streamline microservices integration, and optimize backend server performance for scalable enterprise solutions.`,
+    technologies: ["AWS", "SpringBoot", "Kafka", "Docker", "Kubernetes", "Microservices", "OracleDB", "Jenkins", "Apache Kafka", "JUnit", "Git"]
+    ,
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "SEP 2017 - MAY 2019",
+    role: "Chief Student Technology Officer",
+    company: "CIIE at KL University",
+    description: `Center for Innovation, Incubation & Enterprenuership(CIIE) as a CSTO I contributed in transforming academic research into successful tech-driven startups while fostering a culture of entrepreneurship and innovation on campus.`,
+    technologies: ["Leadership", "IOT", "VLSI-Design", "Embedded-Systems", "WebDevelopment", "ImageProcessing"],
   },
+  
 ];
 
 export const PROJECTS = [
